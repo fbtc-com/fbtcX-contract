76f8d374dee08149246fdc23d301d68aba7d6e67
idyllsss <137248235+idyllsss@users.noreply.github.com>
2024-10-28 11:08:02 +0800

    update

--- src/FBTCRateProvider.sol
+++ src/FBTCRateProvider.sol
@@ -13,28 +13,39 @@ contract RateProvider is AccessControl {
 
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
 
-    event TotalSupplyFbtcUpdated(uint256 newTotalSupplyFbtc);
-    event TotalBtcInProtocolUpdated(uint256 newTotalBtcInProtocol);
+    // Event declarations with old and new values
+    event TotalSupplyFbtcUpdated(uint256 indexed oldTotalSupplyFbtc, uint256 indexed newTotalSupplyFbtc);
+    event TotalBtcInProtocolUpdated(uint256 indexed  oldTotalBtcInProtocol, uint256 indexed newTotalBtcInProtocol);
 
     constructor(address owner, address operator) {
+
+        require(owner != address(0), "Owner address cannot be 0");
+        require(operator != address(0), "Operator address cannot be 0");
 
         _grantRole(DEFAULT_ADMIN_ROLE,owner);
         _grantRole(OPERATOR_ROLE,operator);
     }
 
-    // Set the total supply of FBTC
+    // Set the total supply of all FBTC
     function setTotalSupplyFbtc(uint256 _totalSupplyFbtc) external onlyRole(OPERATOR_ROLE) {
         require(_totalSupplyFbtc > 0, "Total supply of FBTC must be greater than 0");
+
+        uint256 oldTotalSupplyFbtc = totalSupplyFbtc;
         totalSupplyFbtc = _totalSupplyFbtc;
-        emit TotalSupplyFbtcUpdated(totalSupplyFbtc);
+
+        emit TotalSupplyFbtcUpdated(oldTotalSupplyFbtc, totalSupplyFbtc);
     }
 
     // Set the total BTC amount in the protocol
     function setTotalBtcInProtocol(uint256 _totalBtcInProtocol) external onlyRole(OPERATOR_ROLE) {
         require(_totalBtcInProtocol > 0, "Total BTC in protocol must be greater than 0");
+
+        uint256 oldTotalBtcInProtocol = totalBtcInProtocol;
         totalBtcInProtocol = _totalBtcInProtocol;
-        emit TotalBtcInProtocolUpdated(totalBtcInProtocol);
+
+        emit TotalBtcInProtocolUpdated(oldTotalBtcInProtocol, totalBtcInProtocol);
     }
+
 
     // Get the ratio between totalSupplyFbtc and totalBtcInProtocol
     function getRate() external view returns (uint256) {
